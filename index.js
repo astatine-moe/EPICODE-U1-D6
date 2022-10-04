4bccd808abe077f81b5beb2f7365ad3442630f9f
Morgan Evans <morgan@morganevans.dev>
2022-10-04 16:50:04 +0100

    added optional middle name handler

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let firstName = `Morgan`,
+    middleName = ``,
     lastName = `Evans`;
 
 let age = 23,
@@ -7,7 +8,11 @@ let age = 23,
 let birthYear = currentYear - age;
 
 const homework = () => {
-    console.log(`Hello, my name is ${firstName} ${lastName}.`);
+    console.log(
+        `Hello, my name is ${firstName}${
+            middleName ? ` ${middleName} ` : " " //if middleName exists, output it, else ignore it
+        }${lastName}.`
+    );
     console.log(`I was born in ${birthYear}.`);
 };
 
