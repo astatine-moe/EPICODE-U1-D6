254ca72be5e46a9fe4297ac352ea0a34776e6905
Morgan Evans <morgan@morganevans.dev>
2022-10-04 12:03:09 +0100

    Commited homework files

---
+++ index.js
@@ -0,0 +1,12 @@
+let firstName = `Morgan`;
+let middleName = `Robert`;
+let lastName = `Evans`;
+
+console.log(`Hello, my name is ${firstName} ${middleName} ${lastName}.`);
+
+let age = 23;
+let currentYear = new Date().getFullYear();
+
+let birthYear = currentYear - age;
+
+console.log(`I was born in ${birthYear}.`);
